c1e7c42c56662d93a311223bca6ea8660b76580a
sfxcode <tom@sfxcode.com>
2022-04-30 00:26:52 +0200

    core(cleanup): fix linting errors

--- components/AdvertiseBox.vue
+++ components/AdvertiseBox.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
 <template>
   <div class="mb-4 px-5">
     <span class="p-3 shadow-2 mb-1 inline-block" style="border-radius: 10px">
-      <div :class="'text-2xl text-' + color + ' i-' + icon" />
+      <span :class="'text-2xl text-' + color + ' i-' + icon" />
     </span>
     <div :class="'mb-3 text-2xl text-' + color">
       {{ header }}
