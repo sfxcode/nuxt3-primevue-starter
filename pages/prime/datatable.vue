98aa54871e354c60ac116a372143100fcdc136d4
sfxcode <tom@sfxcode.com>
2022-05-26 18:20:43 +0200

    feat(Nuxt): useFetch in datatable

--- pages/prime/datatable.vue
+++ pages/prime/datatable.vue
@@ -1,5 +1,4 @@
 <script setup lang='ts'>
-import consola from 'consola'
 import { FilterMatchMode } from 'primevue/api'
 import { useDataTable } from '~/composables/primevue/dataTable'
 
@@ -12,57 +11,68 @@ filters.value = {
   inventoryStatus: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
 }
 
-const { data: products, refresh: refreshProducts } = await useAsyncData('products', () => $fetch('/data/products.json'), { server: false })
+const { data, pending, error, refresh } = await useFetch(
+  '/data/products.json',
+)
 
-watch(products, (newProducts) => {
-  consola.debug(`number of products loaded: ${newProducts.data.length}`)
-  tableData.value = newProducts.data
+watch(data, () => {
+  tableData.value = data.value.data
 })
 
 onMounted(async () => {
-  refreshProducts()
+  refresh()
 })
 </script>
 
 <template>
   <div class="card">
     <h5>DataTable Example</h5>
-
-    <DataTable
-      ref="dataTableRef" v-model:filters="filters" :value="tableData"
-      data-key="name" :global-filter-fields="['name', 'code', 'inventoryStatus']"
-      striped-rows
-      :paginator="true" :rows="8"
-      paginator-template="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
-      :rows-per-page-options="[8, 15, 50]"
-      current-page-report-template="Showing {first} to {last} of {totalRecords}"
-    >
-      <template #header>
-        <div class="datatable-header">
-          <div class="flex justify-between">
-            <span class="text-xl">Products</span>
-            <span class="p-input-icon-left">
-              <i class="pi pi-search" />
-              <InputText v-model="filters.global.value" placeholder="Globale Suche" />
-            </span>
+    <div v-if="error">
+      <h5 class="text-red-500">
+        Error
+      </h5>
+      <span class="text-red-500">{{ error }}</span>
+    </div>
+    <div v-if="pending">
+      <h6>Loading ...</h6>
+    </div>
+    <div v-else>
+      <DataTable
+        ref="dataTableRef" v-model:filters="filters" :value="tableData"
+        data-key="name" :global-filter-fields="['name', 'code', 'inventoryStatus']"
+        striped-rows
+        :paginator="true" :rows="8"
+        paginator-template="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+        :rows-per-page-options="[8, 15, 50]"
+        current-page-report-template="Showing {first} to {last} of {totalRecords}"
+      >
+        <template #header>
+          <div class="datatable-header">
+            <div class="flex justify-between">
+              <span class="text-xl">Products</span>
+              <span class="p-input-icon-left">
+                <i class="pi pi-search" />
+                <InputText v-model="filters.global.value" placeholder="Globale Suche" />
+              </span>
+            </div>
           </div>
-        </div>
-      </template>
-      <template #empty>
-        No Data Found.
-      </template>
-      <Column field="name" header="Name" :sortable="true" />
-      <Column field="code" header="Code" :sortable="true" />
-      <Column field="price" header="Price" :sortable="true" />
-      <Column field="inventoryStatus" header="Status" :sortable="true" />
-      <template #footer>
-        <div class="flex justify-between">
-          <span class="text-2xl">{{ tableData ? tableData.length : 0 }} Products</span>
-          <Button icon="pi pi-external-link" label="Export" @click="exportCSV" />
-        </div>
-      </template>
-      <template #paginatorRight />
-    </DataTable>
+        </template>
+        <template #empty>
+          No Data Found.
+        </template>
+        <Column field="name" header="Name" :sortable="true" />
+        <Column field="code" header="Code" :sortable="true" />
+        <Column field="price" header="Price" :sortable="true" />
+        <Column field="inventoryStatus" header="Status" :sortable="true" />
+        <template #footer>
+          <div class="flex justify-between">
+            <span class="text-2xl">{{ tableData ? tableData.length : 0 }} Products</span>
+            <Button icon="pi pi-external-link" label="Export" @click="exportCSV" />
+          </div>
+        </template>
+        <template #paginatorRight />
+      </DataTable>
+    </div>
   </div>
 </template>
 
