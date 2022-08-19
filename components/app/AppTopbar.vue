e240a9bb4171ad0289c30f0eafd4d1cf86bd2672
sfxcode <tom@sfxcode.com>
2022-08-19 23:41:58 +0200

    refactor(nuxt-primevue): Use 0.6.4
    PrimeVue 3.6.1

--- components/app/AppTopbar.vue
+++ components/app/AppTopbar.vue
@@ -62,49 +62,51 @@ function onMenuToggle(event: any) {
         </button>
       </li>
     </ul>
-    <OverlayPanel id="overlay_panel" ref="op" append-to="body" style="width: 200px">
-      <div class="field-radiobutton">
-        <RadioButton
-          id="saga" v-model="themeStore.themeName" name="layoutColorMode" value="saga"
-          @change="themeStore.setLight()"
-        />
-        <label>Light</label>
-      </div>
-      <div class="field-radiobutton">
-        <RadioButton
-          id="vela" v-model="themeStore.themeName" name="layoutColorMode" value="vela"
-          @change="themeStore.setDim()"
-        />
-        <label>Dim</label>
-      </div>
-      <div class="field-radiobutton">
-        <RadioButton
-          id="arya" v-model="themeStore.themeName" name="layoutColorMode" value="arya"
-          @change="themeStore.setDark()"
-        />
-        <label>Dark</label>
-      </div>
+    <client-only>
+      <OverlayPanel id="overlay_panel" ref="op" append-to="body" style="width: 200px">
+        <div class="field-radiobutton">
+          <RadioButton
+            id="saga" v-model="themeStore.themeName" name="layoutColorMode" value="saga"
+            @change="themeStore.setLight()"
+          />
+          <label>Light</label>
+        </div>
+        <div class="field-radiobutton">
+          <RadioButton
+            id="vela" v-model="themeStore.themeName" name="layoutColorMode" value="vela"
+            @change="themeStore.setDim()"
+          />
+          <label>Dim</label>
+        </div>
+        <div class="field-radiobutton">
+          <RadioButton
+            id="arya" v-model="themeStore.themeName" name="layoutColorMode" value="arya"
+            @change="themeStore.setDark()"
+          />
+          <label>Dark</label>
+        </div>
 
-      <h6>Primary Color</h6>
-      <div class="flex">
-        <div
-          style="width:2rem;height:2rem;border-radius:6px" class="bg-blue-500 mr-3 cursor-pointer"
-          @click="themeStore.setColor('blue')"
-        />
-        <div
-          style="width:2rem;height:2rem;border-radius:6px" class="bg-green-500 mr-3 cursor-pointer"
-          @click="themeStore.setColor('green')"
-        />
-        <div
-          style="width:2rem;height:2rem;border-radius:6px" class="bg-yellow-300 mr-3 cursor-pointer"
-          @click="themeStore.setColor('orange')"
-        />
-        <div
-          style="width:2rem;height:2rem;border-radius:6px" class="bg-purple-500 cursor-pointer"
-          @click="themeStore.setColor('purple')"
-        />
-      </div>
-    </OverlayPanel>
+        <h6>Primary Color</h6>
+        <div class="flex">
+          <div
+            style="width:2rem;height:2rem;border-radius:6px" class="bg-blue-500 mr-3 cursor-pointer"
+            @click="themeStore.setColor('blue')"
+          />
+          <div
+            style="width:2rem;height:2rem;border-radius:6px" class="bg-green-500 mr-3 cursor-pointer"
+            @click="themeStore.setColor('green')"
+          />
+          <div
+            style="width:2rem;height:2rem;border-radius:6px" class="bg-yellow-300 mr-3 cursor-pointer"
+            @click="themeStore.setColor('orange')"
+          />
+          <div
+            style="width:2rem;height:2rem;border-radius:6px" class="bg-purple-500 cursor-pointer"
+            @click="themeStore.setColor('purple')"
+          />
+        </div>
+      </OverlayPanel>
+    </client-only>
   </div>
 </template>
 
--- histoire.setup.ts
+++ histoire.setup.ts
@@ -1,8 +1,7 @@
-import { defineSetupVue3 } from '@histoire/plugin-vue'
-
-export const setupVue3 = defineSetupVue3(() => {
-  // workaround -  unocss integration from nuxt did not work ...
+export function setupVue3({ app }) {
+  app.provide('test', 'hello')
+  // app.use(...)
   const externalScript = document.createElement('script')
   externalScript.setAttribute('src', 'https://cdn.jsdelivr.net/npm/@unocss/runtime/attributify.global.js')
   document.head.appendChild(externalScript)
-})
+}
