fd7c0feb27b678ebc82f0cd73f916e8af7868cc0
sfxcode <tom@sfxcode.com>
2023-03-10 13:45:36 +0100

    fix(Toast): Toast not working in DEV mode
    Added transpile to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,4 +45,7 @@ export default defineNuxtConfig({
     '@sfxcode/formkit-primevue/dist/sass/formkit-prime-inputs.scss',
     '@sfxcode/formkit-primevue/dist/sass/formkit-primevue.scss',
   ],
+  build: {
+    transpile: ['nuxt', 'primevue'],
+  },
 })
