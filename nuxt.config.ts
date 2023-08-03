76d2cd814602942035f8be0581b847fdc209b4d8
sfxcode <tom@sfxcode.com>
2023-08-03 09:49:26 +0200

    core(update): update all dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,17 +38,6 @@ export default defineNuxtConfig({
     vueI18n: './vue-i18n.options.ts'
 
   },
-  unocss: {
-    uno: true,
-    attributify: true,
-    preflight: false,
-    icons: {
-      scale: 1.2
-    },
-    shortcuts: [
-      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50']
-    ]
-  },
   primevue: {
     config: {
       ripple: true
---
+++ uno.config.ts
@@ -0,0 +1,27 @@
+// uno.config.ts
+import {
+  defineConfig, presetAttributify, presetIcons,
+  presetTypography, presetUno, presetWebFonts,
+  transformerDirectives, transformerVariantGroup
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [
+    ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50']
+  ],
+  theme: {
+    colors: {
+      // ...
+    }
+  },
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography()
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup()
+  ]
+})
