15a81e1bddf24df5e0b1179a777113b03b63af42
sfxcode <tom@sfxcode.com>
2024-02-02 19:14:04 +0100

    core(update): nuxt 3.10

--- pages/index.vue
+++ pages/index.vue
@@ -35,7 +35,7 @@ import AdvertiseBox from '@/components/AdvertiseBox.vue'
         Typesafe by default
       </AdvertiseBox>
 
-      <AdvertiseBox header="Nuxt 3.9" icon="prime-check-circle" color="green-400">
+      <AdvertiseBox header="Nuxt 3.10" icon="prime-check-circle" color="green-400">
         Pages, Layouts, ...
       </AdvertiseBox>
       <AdvertiseBox header="Nuxt Content 2.10" icon="prime-check-circle" color="green-400">
