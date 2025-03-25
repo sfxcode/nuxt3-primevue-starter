6620d7accf546a2a235d1532d198541fa16160e2
sfxcode <tom@sfxcode.com>
2025-03-26 00:24:20 +0100

    style(home): fix shadow

--- app/components/AdvertiseBox.vue
+++ app/components/AdvertiseBox.vue
@@ -16,8 +16,8 @@ defineProps({
 </script>
 
 <template>
-  <div class="mb-4 px-5">
-    <span class="shadow-2 mb-1 p-3 inline-block" style="border-radius: 10px">
+  <div class="mb-4 p-4 px-5 shadow-xs">
+    <span class="mb-1 p-3 inline-block" style="border-radius: 10px">
       <span :class="`text-2xl text-${color} i-${icon}`" />
     </span>
     <div :class="`mb-3 text-2xl text-${color}`">
